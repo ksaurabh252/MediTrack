d1667954b5f53a8f3073546fabf2bdba77b36334
swagata12 <kunal712701@gmail.com>
2025-04-24 20:57:16 +0530

    fixed the register button

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -84,6 +84,20 @@ export default function Login() {
             >
               Sign in
             </button>
+
+            {error && (
+              <>
+                {" "}
+                <br />
+                <button
+                  type="button"
+                  disabled={loading}
+                  className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  <a href="/register">Don't have an account? Register</a>
+                </button>
+              </>
+            )}
           </div>
         </form>
       </div>
