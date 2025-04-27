e5c455b789b3e9828c1fd82f9e8b21911d3050d7
Divyansh <divyanshchandel16@gmail.com>
2025-04-27 16:51:43 +0530

    AI interction popup

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import AIInteractionPopup from './popup/popup.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <AIInteractionPopup>
     <App />
+    </AIInteractionPopup>
   </React.StrictMode>,
 )
